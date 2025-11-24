e4d84879b3dc799ba2562f7e9023daee3ccc74fb
Builtbyjb <awotideajibola@gmail.com>
2025-11-24 12:48:34 -0500

    addition now works

--- Makefile
+++ Makefile
@@ -1,3 +1,3 @@
 dev:
-	as calasm.s -o calasm.o && \
-	ld calasm.o -o calasm -l System -syslibroot `xcrun -sdk macosx --show-sdk-path` -e _main -arch arm64
+	as calculator.s -o calculator.o && \
+	ld calculator.o -o calculator -l System -syslibroot `xcrun -sdk macosx --show-sdk-path` -e _main -arch arm64
--- calculator.s
+++ calculator.s
@@ -7,56 +7,53 @@
 _main:
     cmp     x0, #4 // Check if the argc is equal to 4
     b.ne   _invalid_argument
-
-    ldr     x2, [x1, #8] // First command line argument
+    // First command line argument
+    ldr     x2, [x1, #8]
     bl      _str_to_int
-    ldr     w0, [x2]
-
-    ldr     x3, [x1, #16] // Second command line argument
-    ldrb    w3, [x3] // Store operator
-
-    ldr     x2, [x1, #24] // third command line argument
+    mov     w0, w2
+    // Second command line argument
+    ldr     x2, [x1, #16]
+    ldrb    w3, [x2] // Store operator
+    // third command line argument
+    ldr     x2, [x1, #24]
     bl      _str_to_int
-    ldr     w1, [x2]
+    mov     w7, w2
 
-    // Compare operators
+_compare:  // Compare operators
     cmp     w3, #'+'
     b.eq    _addition
-
     cmp     w3, #'*'
     b.eq    _multiplication
-
     cmp     w3, #'-'
     b.eq    _subtraction
-
     cmp     w3, #'/'
     b.eq    _division
 
 _addition:
-    add     w0, w0, w1
+    add     w0, w0, w7
     bl      _int_to_str
     b       _print_value
 
 _subtraction:
-    sub     w0, w0, w1
+    sub     w0, w0, w7
     bl      _int_to_str
     b       _print_value
 
 _division:
     cmp     w1, #0
     b.eq    _zero_division_error
-    udiv    w0, w0, w1
+    udiv    w0, w0, w7
     bl      _int_to_str
     b       _print_value
 
 _multiplication:
-    mul     w0, w0, w1
+    mul     w0, w0, w7
     bl      _int_to_str
     b       _print_value
 
 _invalid_argument:
     adr     x1, error_msg
-    mov     x19, #50
+    mov     x19, #48
     b       _print_value
 
 _zero_division_error:
@@ -86,7 +83,6 @@ _str_to_int:
     mov     x10, #0 // Accumulator (result)
     mov     x11, #10 // Constant
     mov     x12, #0 // Sign flag (0 = +, 1 = -)
-
     // Check for negative sign
     ldrb    w9, [x2] // Load first byte
     cmp     w9, #45 // Check if the first byte (character) is a negative sign ('-' is 45 is ASCII)
@@ -95,13 +91,24 @@ _str_to_int:
     add     x2, x2, #1 // Move the pointer to the next byte (Character)
 
 _int_convert_loop:
+    ldrb    w9, [x2], #1
+    cbz     w9, _end_int_convert_loop
+    madd    x10, x10, x11, x9 // x9 equals w9 because the upper bits are zeroed out
+    b       _int_convert_loop
 
+_end_int_convert_loop:
+    cmp     x12, #1
+    b.ne    _return_int
+    neg     x10, x10
+
+_return_int:
+    sub     x2, x10, #48 // Subtract 48 from x10 to get integer value
     ret
 
  _int_to_str:
     mov     x19, #0 // character count
     mov     w2, #10 // divisor
-    sub     sp, sp, #64 // Allocate memory on the stack
+    sub     sp, sp, #128 // Allocate memory on the stack
 
  _str_convert_loop:
     udiv    w4, w0, w2
@@ -111,7 +118,6 @@ _int_convert_loop:
     add     x19, x19, #1
     mov     w0, w4
     cbnz    w0, _str_convert_loop
-
     mov     x4, #0
     sub     x18, x19, #1
 
@@ -122,7 +128,6 @@ _int_convert_loop:
     sub     x18, x18, #1
     cmp     x4, x19
     b.lt    _copy
-
     add     sp, sp, #64
     ret
 
